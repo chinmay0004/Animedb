985e9bf5c69f4066457f516353ed63cbd9d9787d
unknown <chinmaylokhande30@gmail.com>
2026-07-26 12:59:37 +0530

    database migrations

---
+++ Server/db/animedb.js
@@ -0,0 +1,28 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const {Pool} = pg;
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+
+})
+
+pool.connect()
+.then(()=> {
+    console.log("postgres connected")
+})
+.catch((err) => {
+    console.log(err)
+})
+
+
+
+export default pool
+
---
+++ Server/db/migrations/animeup.sql
@@ -0,0 +1,65 @@
+CREATE TABLE anime(
+    id UUID PRIMARY KEY,
+    external_id INTEGER UNIQUE NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT,
+    status TEXT,
+    episodes INTEGER,
+    aired_from DATE,
+    aired_to DATE,
+    image_url TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE genres(
+   id UUID PRIMARY KEY,
+   name TEXT NOT NULL UNIQUE,
+   created_at TIMESTAMP DEFAULT NOW(),
+   updated_at TIMESTAMP DEFAULT NOW() 
+);
+
+CREATE TABLE anime_genres(
+    anime_id UUID NOT NULL,
+    genre_id UUID NOT NULL,
+    PRIMARY KEY(anime_id, genre_id),
+    FOREIGN KEY(anime_id) REFERENCES anime(id) ON DELETE CASCADE,
+    FOREIGN KEY(genre_id) REFERENCES genres(id) ON DELETE CASCADE
+);
+
+CREATE TABLE studios(
+    id UUID PRIMARY KEY,
+    external_id INTEGER UNIQUE NOT NULL,
+    name TEXT UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE anime_studios(
+    anime_id UUID NOT NULL,
+    studio_id UUID NOT NULL,
+    PRIMARY KEY(anime_id, studio_id),
+    FOREIGN KEY (anime_id) REFERENCES anime(id) ON DELETE CASCADE,
+    FOREIGN KEY (studio_id) REFERENCES studios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE characters(
+    id UUID PRIMARY KEY,
+    external_id INTEGER UNIQUE NOT NULL,
+    anime_id UUID NOT NULL,
+    name TEXT NOT NULL,
+    image_url TEXT,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(anime_id) REFERENCES anime(id) ON DELETE CASCADE
+);
+
+CREATE TABLE users(
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
