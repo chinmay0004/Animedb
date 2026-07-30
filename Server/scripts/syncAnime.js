79f253d5c6e8f8eb4d0646f2f81bb7230b462d33
unknown <chinmaylokhande30@gmail.com>
2026-07-30 12:26:07 +0530

    pagination for sync database

--- Server/scripts/syncAnime.js
+++ Server/scripts/syncAnime.js
@@ -5,31 +5,36 @@ import insertAnime from "../repository/animeRepository.js";
 
 
 async function syncAnime() {
-    const response = await fetch('https://api.jikan.moe/v4/top/anime')
-    const result = await response.json()
 
+    for(let page = 1; page <= 3; page++){
+        console.log(`fetching page: ${page}`)
+        const response = await fetch( `https://api.jikan.moe/v4/top/anime?page=${page}`)
 
-    if (!response.ok) {
+        if (!response.ok) {
         console.log(`Request failed: ${response.status}`);
-        return;
+        continue;
     }
 
-    for(const rawData of result.data){
+        const result = await response.json()
+
+        for(const rawData of result.data){
         const validationResult = validateData(rawData)
 
         if(!validationResult.isValid){
         console.log(validationResult.errors);
         continue;
-    }
+        }
 
         const transformedAnime = transformData(validationResult.data)
 
         await insertAnime(transformedAnime);
 
-        console.log(`Inserted: ${transformedAnime.title}`); 
+        console.log(`Inserted: ${transformedAnime.title}`);
+       }
     }
-   console.log("sync completed, data is transformed")
-}
+     console.log("sync completed, data is transformed")
+    }
+   
 
 syncAnime()
 
